5ac63e1fafc8462a18bd17e5eb019c589e49bc80
tamanishi <tamanishi@gmail.com>
2016-07-02 09:00:53 +0900

    initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,20 @@
+var app = angular.module('App', []);
+
+app.service('dataService', ['$http', function($http) {
+
+  var url = '/episodes.json'
+
+  this.getEpisodes = function() {
+    return $http.get(url);
+  };
+
+}]);
+
+app.controller('mainCtrl', ['$scope', 'dataService', function($scope, dataService) {
+  dataService.getEpisodes()
+    .then(function(response) {
+      $scope.episodes = response.data.episodes;
+    }, function(error) {
+
+    });
+}]);
