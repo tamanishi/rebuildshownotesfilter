9d62ce0749651e3736f78145b444bfbb4783bd3d
tamanishi <tamanishi@gmail.com>
2016-07-02 09:09:19 +0900

    adjust json url

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ var app = angular.module('App', []);
 
 app.service('dataService', ['$http', function($http) {
 
-  var url = '../episodes.json'
+  var url = 'https://tamanishi.github.io/rebuildshownotesfilter/episodes.json'
 
   this.getEpisodes = function() {
     return $http.get(url);
